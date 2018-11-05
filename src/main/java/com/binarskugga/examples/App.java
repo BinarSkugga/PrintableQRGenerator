1bebaf47d9bc2fef48d9ae12a3bd7c3191516b3f
Charles Smith <binarskugga@gmail.com>
2018-11-05 00:44:54 -0500

    Cleaned up examples.

--- src/main/java/com/binarskugga/examples/App.java
+++ src/main/java/com/binarskugga/examples/App.java
@@ -2,6 +2,7 @@ package com.binarskugga.examples;
 
 import com.binarskugga.*;
 import com.itextpdf.text.*;
+import io.nayuki.qrcodegen.*;
 
 import java.util.*;
 import java.util.List;
@@ -12,21 +13,15 @@ public class App {
 		// This example works with the Avery template #5167
 		QRSerialGenerator<UUID> generator = new ExampleQRGenerator();
 
-		// All rectangles take values in inches.
-		// This is the size of the sheet of paper.
 		generator.setPageSize(new RectangleReadOnly(8.5f, 11f));
-		// This is the margins around your page.
 		generator.setOutsetMargin(new RectangleReadOnly(5f / 16f, 0.5f));
-		// This is the margins between your stickers.
 		generator.setInsetMargin(new RectangleReadOnly(5f / 16f, 0));
-		// This is the size of a single sticker.
 		generator.setStickerSize(new RectangleReadOnly(1.75f, 0.5f));
 
-		// Amount of stickers on one line.
 		generator.setLineSize(4);
-
-		// Generate 800 stickers (fitting on exactly 10 pages) into "simple-generator.pdf".
 		generator.setBatchSize(800);
+		generator.setEcc(QrCode.Ecc.LOW);
+
 		generator.generate("simple-generator.pdf");
 
 
@@ -40,20 +35,13 @@ public class App {
 		);
 		QRSerialCollectionGenerator<SimplePojo> collectionGenerator = new ExampleQRGeneratorCollection(items);
 
-		// All rectangles take values in inches.
-		// This is the size of the sheet of paper.
 		collectionGenerator.setPageSize(new RectangleReadOnly(8.5f, 11f));
-		// This is the margins around your page.
 		collectionGenerator.setOutsetMargin(new RectangleReadOnly(5f / 16f, 0.5f));
-		// This is the margins between your stickers.
 		collectionGenerator.setInsetMargin(new RectangleReadOnly(5f / 16f, 0));
-		// This is the size of a single sticker.
 		collectionGenerator.setStickerSize(new RectangleReadOnly(1.75f, 0.5f));
 
-		// Amount of stickers on one line.
 		collectionGenerator.setLineSize(4);
-
-		// Generate all collection stickers into simple-collection-generator.pdf.
+		collectionGenerator.setLabelFontSize(10);
 		collectionGenerator.generate("simple-collection-generator.pdf");
 	}
 
